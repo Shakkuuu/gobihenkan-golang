0906fa07d479df7b3705b4fea7afa10a45684747
Shakkuuu <shakku0521@gmail.com>
2023-01-20 10:18:48 +0900

    対応言葉増加

--- gobihenkan/gobihenkan.go
+++ gobihenkan/gobihenkan.go
@@ -6,17 +6,43 @@ import (
 )
 
 var keyword = map[string]string{
-	"まだ": "まだなのだ",
-	"した": "したのだ",
-	"った": "ったのだ",
-	"です": "なのだ",
-	"よう": "ようなのだ",
-	"な":  "ななのだ",
+	"まだ":   "まだなのだ",
+	"した":   "したのだ",
+	"った":   "ったのだ",
+	"です":   "なのだ",
+	"よう":   "ようなのだ",
+	"します":  "するのだ",
+	"される":  "されるのだ",
+	"されます": "されるのだ",
+	"られる":  "れるのだ",
+	"ます":   "ますのだ",
 }
 
-func changegobi(w string) {
+func ChangeGobi(w string) {
+	var i bool
 	for key, value := range keyword {
 		w = strings.ReplaceAll(w, key, value)
 	}
+	i = strings.HasSuffix(w, "のだ")
+	if i {
+		fmt.Println(w)
+		return
+	}
+
+	o := strings.HasSuffix(w, "る")
+	if o {
+		w += "のだ"
+		fmt.Println(w)
+		return
+	}
+
+	s := strings.HasSuffix(w, "た")
+	if s {
+		w += "のだ"
+		fmt.Println(w)
+		return
+	}
+
+	w += "なのだ"
 	fmt.Println(w)
 }
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/Shakkuuu/gobihenkan-golang/gobihenkan"
+)
+
+func main() {
+	var word string
+	fmt.Scanln(&word)
+	gobihenkan.ChangeGobi(word)
+}
